5f927fa272a824e3c8e7f6353b6dad066ed2a7b0
Kalrul <prizsabertler@gmail.comgit config --global user.email prizsabertler@gmail.com>
2022-04-19 13:15:45 -0700

    error correction

--- print_custom.c
+++ print_custom.c
@@ -79,7 +79,8 @@ int print_rot13(va_list l, flags_t *f)
 	(void)f;
 	for (j = 0; s[j]; j++)
 	{
-		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z');
+		if
+		(s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z');
 		_putchar(s[j]);
 		else
 		{
